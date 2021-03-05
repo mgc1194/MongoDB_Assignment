73051d3e6bb8611219bdded2d374223468446ecb
Mario Gil <mario.gil@urjc.es>
2021-03-05 17:19:12 -0600

    added new scipts and json files

--- import_to_mongo.py
+++ import_to_mongo.py
@@ -1,65 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
 import json
 import time
+import glob
 from pymongo import MongoClient
 
 
 def main():
     # Ruta destino
-    client = MongoClient()
+    #client = MongoClient()
     client = MongoClient('localhost', 27017)
 
-    # Nombre de la colección destino
+    # Collection name
     db = client.Publications
+    # The collection is
+    db.Publications.drop()
 
-    # Ruta y nombre del archivo origen
-    json_dir = "./data/"
-    json_name = "dblp.json"
+    file_list = glob.glob('data/dblp.*.json')
 
-    # Se lee el archivo origen.
-    # 1554543526.0884762
-    start = time.time()
-    with open(json_dir + json_name) as file:
-        Json = json.load(file)
-    file.close()
-    end = time.time()
-    # 1554544637.8074813
+    # Loading json files.
+    for json_file in file_list:
+        print('Reading file %s' % json_file)
+        start = time.time()
+        with open(json_file) as file:
+            Json = json.load(file)
+        file.close()
+        end = time.time()
 
-    # Se seleccionan las publicaciones y no otra información
-    articles = Json['dblp']
+        # Se seleccionan las publicaciones y no otra información
+        articles = Json['dblp']
 
-    # Como se da por hecho que la colección es nueva y no una actualización ni
-    # una insercción adicional de la misma, se elimina previamente por si hubiera
-    # alguna antigua.
+        # Se crea la colección.
+        articles_colletion = db.Publications
 
-    db.publications.drop()
+        start2 = time.time()
 
-    # Se crea la colección.
-    articles_colletion = db.publications
+        # Se van recorriendo los documentos e insertanto en la colección. Se
+        # transforma el campo year a tipo entero.
+        try:
+            for journal in articles['article']:
+                if 'year' in journal:
+                    journal['year'] = int(float(journal['year']))
+                journal['type'] = 'article'
+                article_id = articles_colletion.insert_one(journal)
+        except:
+            print('No journal publications in this file.')
+        try:
+            for book in articles['incollection']:
+                if 'year' in book:
+                    book['year'] = int(float(book['year']))
+                book['type'] = 'incollection'
+                article_id = articles_colletion.insert_one(book)
+        except:
+            print('No book publications in this file.')
+        try:
+            for conference in articles['inproceedings']:
+                if 'year' in conference:
+                    conference['year'] = int(float(conference['year']))
+                conference['type'] = 'inproceedings'
+                article_id = articles_colletion.insert_one(conference)
+        except:
+            print('No conference publications in this file.')
 
-    start2 = time.time()
-    # 1554545651.7335484
 
-    # Se van recorriendo los documentos e insertanto en la colección. Se
-    # transforma el campo year a tipo entero.
-    for revista in articles['article']:
-        if 'year' in revista:
-            revista['year'] = int(float(revista['year']))
-        revista['type'] = 'article'
-        article_id = articles_colletion.insert_one(revista)
-
-    for libro in articles['incollection']:
-        if 'year' in libro:
-            libro['year'] = int(float(libro['year']))
-        libro['type'] = 'incollection'
-        article_id = articles_colletion.insert_one(libro)
-    for congreso in articles['inproceedings']:
-        if 'year' in congreso:
-            congreso['year'] = int(float(congreso['year']))
-        congreso['type'] = 'inproceedings'
-        article_id = articles_colletion.insert_one(congreso)
-
-    end2 = time.time()
-    # 1554550267.6431792
 
 
 if __name__ == '__main__':
